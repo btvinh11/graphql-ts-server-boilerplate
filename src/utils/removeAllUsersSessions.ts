71755546f93ae871f72b802820940db71f300d38
vinhbhn <bvinhhn@gmail.com>
2020-10-16 12:22:52 +0700

    tslint to eslint

--- src/utils/removeAllUsersSessions.ts
+++ src/utils/removeAllUsersSessions.ts
@@ -9,7 +9,7 @@ export const removeAllUsersSessions = async (userId: string, redis: Redis) => {
   );
 
   const promises = [];
-  // tslint:disable-next-line: prefer-for-of
+  // eslint-disable-next-line @typescript-eslint/prefer-for-of
   for (let i = 0; i < sessionIds.length; i++) {
     promises.push(redis.del(`${redisSessionPrefix}${sessionIds[i]}`));
   }
